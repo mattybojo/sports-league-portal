0ec49864af033d47914f22538b1cf7701e659d73
mattybojo <mattbojorquez@gmail.com>
2018-11-09 12:22:05 -0800

    Added basic ng-sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  _opened = false;
+
+  _toggleSidebar() {
+    this._opened = !this._opened;
+  }
 }
